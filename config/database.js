a78883f921e6e44e0654992bd9884391c3167e5a
shayan368 <shayanahmad368@gmail.com>
2026-07-22 11:47:33 +0500

    Configure database for Railway and Aiven

--- config/database.js
+++ config/database.js
@@ -2,13 +2,21 @@ import { Sequelize } from "sequelize";
 import "dotenv/config";
 
 const database = new Sequelize(
-  process.env.DB_NAME || "clinic_management",
-  process.env.DB_USER || "root",
-  process.env.DB_PASSWORD || "",
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST || "localhost",
-    dialect: process.env.DB_DIALECT || "mysql",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
     logging: false,
+
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
 
@@ -19,15 +27,11 @@ const connectDB = async (force = false) => {
 
     await database.sync({ force });
 
-    if (force) {
-      console.log("⚠️  Database synced with force: true — All tables recreated");
-    } else {
-      console.log("✅ Database synced — Tables preserved");
-    }
+    console.log("✅ Database synced");
 
     return true;
   } catch (error) {
-    console.error("❌ Unable to connect to database:", error);
+    console.error(error);
     return false;
   }
 };
